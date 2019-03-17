88f34efb1cd9af5f727a441418d0be6951ebce3c
Gianluca <ganlucavico@gmail.com>
2019-03-17 10:49:11 +0100

    Update Vector.java
    Added length, dot and cross product, scalar multiplication, normalize and rotate

--- Vector.java
+++ Vector.java
@@ -4,11 +4,16 @@ public class Vector {
 	private double Y;
 	private double Z;
 	
-	
 	public Vector(double PosX, double PosY, double PosZ) {
 		this.setX(PosX);
 		this.setY(PosY);
 		this.setZ(PosZ);
+	}
+
+	public Vector(Vector v) {
+		this.setX(v.getX());
+		this.setY(v.getY());
+		this.setZ(v.getZ());
 	}
 	
 	//distance between this vector and vector v.
@@ -29,10 +34,42 @@ public class Vector {
 		return subtract;
 	}
 	
-	
-	public double getZ() {
-		return Z;
+	public double length() {
+		return Math.sqrt(squareLength());
 	}
+
+	public double squareLength() {
+		return X*X + Y*Y + Z*Z;
+	}
+
+	public Vector multiply(double factor) {
+		return new Vector(X * factor, Y * factor, Z * factor);
+	}
+
+	public double dot(Vector v) {
+		return X * v.getX() + Y * v.getY() + Z * v.getZ();
+	}
+
+	public Vector cross(Vector v) {
+		//from cross product formula
+		return new Vector(Y * v.getZ() - Z * v.getY(), Z * v.getX() - X * v.getZ(), X * v.getY() - Y * v.getX());
+	}
+
+	public Vector rotate(Vector axis, double rad) {
+		//Euler angle	-> gimball lock, we need to calculate the three angles
+		//Axis - angle	-> we already have the direction and the angle (from torque momentum)
+		//Quaternion rotation	-> no gimball lock, but we need a new class for quaternions
+
+		//Axis - angle (from formula)
+		Vector v = axis.normalize();
+		return	this.multiply(Math.cos(rad)).sum(v.cross(this).multiply(Math.sin(rad))).sum(v.multiply(v.dot(this) * (1 - Math.cos(rad))));
+	}
+
+	public Vector normalize() {
+		return new Vector(multiply(1 / length()));
+	}
+
+	public double getZ() { return Z; }
 	public void setZ(double z) {
 		Z = z;
 	}
