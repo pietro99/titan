f438a8b97810f713d811410a24403baad57a832f
pietro99 <43954467+pietro99@users.noreply.github.com>
2019-03-17 13:32:21 +0100

    Update Planet.java

--- Planet.java
+++ Planet.java
@@ -1,16 +1,15 @@
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class Planet {
+	private final double g = 6.67408E-11;
 	private String name;
 	private double mass;
 	private Vector position;
 	private Vector velocity;
 	private double radius; 
+	private Vector gravityForce;
 	private Vector acceleration;
-	private Ellipse orbit;
-	private List<Planet> moon = new ArrayList<Planet>();
+	
 	
 	
 	//constructor
@@ -19,12 +18,37 @@ public class Planet {
 		this.velocity = initialVelocity;
 		this.radius = radius;
 		this.mass = mass;
-	}
-	
-	public void addMoon(Planet moon) {
-		this.moon.add(moon);
+		this.acceleration = new Vector(0,0,0);
 	}
 
+	
+	
+	//calculate the gravitational pull
+	public void calculateGravityForce() {
+		for(int i=0; i<SolarSystem.getPlanets().length; i++) {
+			if(!this.equals(SolarSystem.getPlanets()[i])) {
+				Planet p = SolarSystem.getPlanets()[i];
+				Vector direction = this.getPosition().subtract(p.getPosition()).normalize();
+				double mass = p.getMass();
+				double distance = this.position.distance(p.getPosition());
+				double force = g*((this.mass*mass)/Math.pow(distance, 2));
+				Vector forceVector = direction.multiply(force);
+				p.setGravityForce(forceVector);
+				p.addAcceleration();
+		}
+	}
+	}
+	public void addAcceleration() {
+		acceleration = acceleration.sum(gravityForce.multiply(1/mass));
+	}
+	
+	public void updateVelocityAndPosition(double deltaT) {
+		Vector oldVel = new Vector(velocity.getX(),velocity.getY(),velocity.getZ());
+		velocity = velocity.sum(acceleration.multiply(deltaT));
+		Vector averageVel = oldVel.sum(velocity).multiply(0.5);
+		position = position.sum((averageVel).multiply(deltaT));
+		acceleration = new Vector(0,0,0);
+	}
 	public Vector getPosition() {
 		return position;
 	}
@@ -32,4 +56,12 @@ public class Planet {
 	public void setPosition(Vector position) {
 		this.position = position;
 	}
+	
+	public double getMass() {
+		return mass;
+	}
+	public void setGravityForce(Vector force) {
+		gravityForce = force;
+	}
+	
 }
